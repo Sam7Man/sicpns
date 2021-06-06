dc5ef2b109ae14d7ff1373cda9954531fe7bef9b
Samuel Halomoan Manalu <44311690+Sam7Man@users.noreply.github.com>
2021-06-07 04:08:37 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,87 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SI CPNS | Login</title>
-  <!-- Tell the browser to be responsive to screen width -->
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <!-- Bootstrap 3.3.6 -->
-  <link rel="stylesheet" href="{{ url('/assets/bootstrap/css/bootstrap.min.css') }}">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{ url('/assets/plugins/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="{{ url('/assets/plugins/ionicons-2.0.1/css/ionicons.min.css') }}">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{ url('/assets/dist/css/AdminLTE.min.css') }}">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{ url('/assets/plugins/iCheck/square/blue.css') }}">
-
-  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
+	<meta charset="utf-8">
+	<meta name="author" content="">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+	<title>SIT CPNS</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="{{URL::asset('assets/css/my-login.css')}}">
+	<link href="{{URL::asset('assets/img/logo.png')}}" rel="icon">
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-logo">
-    <a href="{{ url('/login') }}"><b>SI</b> CPNS</a>
-  </div>
-  <!-- /.login-logo -->
-  <div class="login-box-body">
-    <p class="login-box-msg">Silahkan login dahulu!</p>
 
-    <form role="form" method="POST" action="{{ route('login') }}">
-      {{ csrf_field() }}
-      <div class="form-group has-feedback {{ $errors->has('email') ? ' has-error' : '' }}">
-        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
-        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
-        @if ($errors->has('email'))
-          <span class="help-block">
-            <strong>{{ $errors->first('email') }}</strong>
-          </span>
-        @endif
-        <!-- <span class="glyphicon glyphicon-envelope form-control-feedback"></span> -->
-      </div>
-      <div class="form-group has-feedback {{ $errors->has('password') ? ' has-error' : '' }}">
-        <input id="password" type="password" class="form-control" name="password" required>
-        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-        @if ($errors->has('password'))
-          <span class="help-block">
-            <strong>{{ $errors->first('password') }}</strong>
-          </span>
-        @endif
-      </div>
-      <div class="row">
-        <div class="col-xs-8">
-          <div class="checkbox icheck">
-            <label>
-              <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-            </label>
-          </div>
-        </div>
-        <!-- /.col -->
-        <div class="col-xs-4">
-          <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
-        </div>
-        <!-- /.col -->
-      </div>
-    </form>
-  </div>
-  <!-- /.login-box-body -->
-</div>
-<!-- /.login-box -->
+<body class="my-login-page" style="background-image: url('/assets/img/squairy_light.png');">
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-md-center h-100">
+				<div class="card-wrapper">
+					<div class="brand">
+						<img src="{{URL::asset('/assets/img/logo.png')}}" alt="logo">
+					</div>
+					<div class="card fat">
+						<div class="card-body">
+							<h4 class="card-title"><center>Silahkan login dahulu!</center></h4>
+							<form method="POST" class="my-login-validation" novalidate="" action="{{ route('login') }}">
+								{{ csrf_field() }}
+								<div class="form-group {{ $errors->has('email') ? ' has-error' : '' }}">
+									<label for="email">Email</label>
+									<input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+									@if ($errors->has('email'))
+									  <span class="help-block">
+										<p style="color: #dc3545;"><b>{{ $errors->first('email') }}</b></p>
+									  </span>
+									@endif
+									<div class="invalid-feedback">
+										<p style="font-size:12px;">Email is invalid</p>
+									</div>
+								</div>
 
-<!-- jQuery 2.2.3 -->
-<script src="{{ url('/assets/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
-<!-- Bootstrap 3.3.6 -->
-<script src="{{ url('/assets/bootstrap/js/bootstrap.min.js') }}"></script>
-<!-- iCheck -->
-<script src="{{ url('/assets/plugins/iCheck/icheck.min.js') }}"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '10%' // optional
-    });
-  });
-</script>
+								<div class="form-group {{ $errors->has('password') ? ' has-error' : '' }}">
+									<label for="password">Password</label>
+									<input id="password" type="password" class="form-control" name="password" required data-eye>
+									@if ($errors->has('password'))
+									  <span class="help-block">
+										<p style="color:#dc3545;">{{ $errors->first('password') }}</p>
+									  </span>
+									@endif
+								    <div class="invalid-feedback">
+								    	<p style="font-size:12px;">Password is required</p>
+							    	</div>
+								</div>
+
+								<div class="form-group">
+									<div class="custom-checkbox custom-control">
+										<input type="checkbox" name="remember" id="remember" class="custom-control-input" {{ old('remember') ? 'checked' : '' }}>
+										<label for="remember" class="custom-control-label">Remember Me</label>
+										{{-- <label for="remember" class="custom-control-label">
+											<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} id="remember" class="custom-control-input"> Remember Me
+										</label> --}}
+									</div>
+								</div>
+
+								<div class="form-group m-0">
+									<button type="submit" class="btn btn-primary btn-block">
+										Login
+									</button>
+								</div>
+								<div class="mt-4 text-center">
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="footer">
+						Copyright &copy; 2021 &mdash; Institut Teknologi Del <br>
+						<a href="{{url('/')}}">Home</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="{{URL::asset('/assets/js/my-login.js')}}"></script><script src="{{ url('/assets/plugins/jQuery/jquery-2.2.3.min.js') }}"></script>
 </body>
 </html>
-
